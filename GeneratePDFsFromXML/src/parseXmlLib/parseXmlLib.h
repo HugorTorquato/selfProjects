c8c85e2a525856a88af7bf3850ee5e7d38739161
Hugo Torquato <hugortorquato@gmail.com>
2024-11-20 14:16:43 -0300

    enabling qt5 to be access onto the container

--- GeneratePDFsFromXML/src/parseXmlLib/parseXmlLib.h
+++ GeneratePDFsFromXML/src/parseXmlLib/parseXmlLib.h
@@ -1,9 +1,18 @@
 #pragma once
 
+//#include "qt5/QtCore/QXmlStreamReader"
+#include <string>
+
+using namespace std;
+
 class testLib{
     public:
 
         int getId();
 
+        string getReaderName();
+
         int id;
+
+        //QXmlStreamReader reader;
 };
